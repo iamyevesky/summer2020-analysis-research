7f6cb6c7d0283b798c33e785d2a1f78ebff166d6
Sena Yevenyo <yevesky2020@gmail.com>
2021-02-11 14:04:43 -0500

    Set up testing environment

---
+++ main/toolstest.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""Tools Test Package
+
+This script contains test classes which provide performs unit tests
+for the classes and exceptions listed in the tools package in the `main` folder.
+
+This script requires that `pandas`, `numpy`, and `matplotlib` are installed
+within the Python environment. 
+
+This program is written with Python version 3.7.3 with Spyder IDE.
+"""
